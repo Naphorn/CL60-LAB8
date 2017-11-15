05f522630be14c3f0f3c3133e7d64ff68a5ed7ab
Naphorn <naphorn.chok@gmail.com>
2017-11-15 22:26:20 +0700

    Zoom out

---
+++ GDIPlus_3/GDIPlus_3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDIPlus_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Bitmap bmp = new Bitmap("D:\\Capture.PNG");
+            Rectangle destrect = new Rectangle(10, 10, bmp.Width / 2, bmp.Height / 2);
+            Rectangle srcrect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            this.SetClientSizeCore(destrect.Width + 20, destrect.Height + 20);
+            g.DrawImage(bmp, destrect, srcrect, GraphicsUnit.Pixel);
+            g.Dispose();
+        }
+    }
+}
